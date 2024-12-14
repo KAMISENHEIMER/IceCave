beeae96fab1f7b2f172cc6b99c1021d3ea33437a
BigManJD <79377649+BigManJD@users.noreply.github.com>
2024-12-14 04:30:19 -0600

    Main Area + box moving first pass
    Adding main area (not done)
    adding box moving script:
    - call "tryPush(Vector2 Direction)" with the direction of which way you want to push.
    ----I believe this function should also be called when it is on an ice tile

---
+++ IceCave/Assets/Pushable.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pushable : MonoBehaviour
+{
+    [SerializeField] private float pushTime;
+    [SerializeField] private Vector2 lerpPosition;
+    [SerializeField] private bool testMove;
+    
+    [SerializeField] private List<Pushable> connectedPushables = new List<Pushable>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+        if(testMove == true) // PLEASE DONT LOOK AT THIS !! D:
+        {
+            if (Input.GetKeyDown("w"))
+            {
+                tryPush(new Vector2(0, 1));
+            }
+            else if (Input.GetKeyDown("a"))
+            {
+                tryPush(new Vector2(-1, 0));
+            }
+            else if (Input.GetKeyDown("s"))
+            {
+                tryPush(new Vector2(0, -1));
+            }
+            else if (Input.GetKeyDown("d"))
+            {
+                tryPush(new Vector2(1, 0));
+            }
+        }
+        
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (lerpPosition != Vector2.zero)
+        {
+            transform.position = Vector2.Lerp(transform.position, lerpPosition, pushTime);
+            if ((Vector2)transform.position == lerpPosition)
+            {
+                lerpPosition = Vector2.zero;
+            }
+        }
+    }
+
+    public void tryPush(Vector2 direction)
+    {
+        connectedPushables = new List<Pushable>();
+
+        connectedPushables.Add(this);
+
+        RaycastHit2D hit = Physics2D.Linecast((Vector2)transform.position, (Vector2)transform.position + direction);
+
+        if (hit)
+        {
+            while (hit.collider != null)
+            {
+                if (hit.collider.transform.tag == "immovables")
+                {
+                    Debug.Log("Can't move!");
+
+                    return;
+                }
+
+
+
+                if (hit.collider.GetComponent<Pushable>() != null)
+                {
+                    connectedPushables.Add(hit.collider.GetComponent<Pushable>());
+
+                    Pushable currentPushable = connectedPushables[connectedPushables.Count - 1];
+
+                    hit = Physics2D.Linecast(currentPushable.transform.position, (Vector2)currentPushable.transform.position + direction);
+                }
+
+                
+            }
+        }
+
+        if(connectedPushables.Count > 0)
+        {
+            foreach (Pushable pushable in connectedPushables)
+            {
+                pushable.doPush(direction);
+            }
+        }
+        
+
+        connectedPushables = new List<Pushable>();
+    }
+
+    public void doPush(Vector2 direction)
+    {
+        lerpPosition = (Vector2)transform.position + direction;
+    }
+}
