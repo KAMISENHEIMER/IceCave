8d7ab7bbad65dc34d6a30b5f17b509ace161e27d
KAMISENHEIMER <therealspydude@gmail.com>
2024-12-16 01:43:38 -0600

    added basic up and down for red button

--- IceCave/Assets/Scripts/Button.cs
+++ IceCave/Assets/Scripts/Button.cs
@@ -5,22 +5,32 @@ public class Button : MonoBehaviour
     // The door that this button opens
     public GameObject door;
     Collider2D doorCollider;
+    
+    Animator animator;
+    Animator doorAnimator;
 
     void Start()
     {
         doorCollider = door.GetComponent<Collider2D>();
+        animator = gameObject.GetComponent<Animator>();
+        doorAnimator = door.GetComponent<Animator>();
     }
     
     void OnTriggerEnter2D(Collider2D otherCollider)
     {
         Debug.Log("Button Pressed");
         doorCollider.enabled = false;
+        animator.SetBool("buttonDown", true);
+        doorAnimator.SetBool("buttonDown", true);
+
     }
     
     void OnTriggerExit2D(Collider2D otherCollider)
     {
         Debug.Log("Button Unpressed");
         doorCollider.enabled = true;
+        animator.SetBool("buttonDown", false);
+        doorAnimator.SetBool("buttonDown", false);
 
     }
 }
