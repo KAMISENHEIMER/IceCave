7dff65ebd77168d59a4bc0fee3f846957b358417
KAMISENHEIMER <therealspydude@gmail.com>
2024-12-15 19:01:27 -0600

    snowflake animations, and making sure the snowflake doesnt show up on a stage it has already been collected on

--- IceCave/Assets/Scripts/Snowflake.cs
+++ IceCave/Assets/Scripts/Snowflake.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class Snowflake : MonoBehaviour
 {
     //holds the amount of snowflakes that have been picked up in order to open the door
     public static int collectedSnowflakes = 0;
+    public static List<int> collectedStages = new List<int>();
     
     public Collider2D collider;
         
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log(collectedSnowflakes);
+        foreach (int stage in collectedStages)
+        {
+            if (stage == SceneManager.GetActiveScene().buildIndex)
+            {
+                Destroy(gameObject);
+                break;
+            }
+        }        
     }
 
     // Update is called once per frame
@@ -24,6 +34,7 @@ public class Snowflake : MonoBehaviour
         collectedSnowflakes++;
         Debug.Log(collectedSnowflakes);
         //display particle effect or snowflake flying off screen here
+        collectedStages.Add(SceneManager.GetActiveScene().buildIndex);
         Destroy(gameObject);
     }
 }
