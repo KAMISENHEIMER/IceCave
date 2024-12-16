59ac29f8458c8b520a3052de332f10771ac236bf
BigManJD <79377649+BigManJD@users.noreply.github.com>
2024-12-16 13:09:03 -0600

    changed player

--- IceCave/Assets/Scripts/PlayerInputs.cs
+++ IceCave/Assets/Scripts/PlayerInputs.cs
@@ -22,7 +22,7 @@ public class PlayerInputs : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("Escape"))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
         }
