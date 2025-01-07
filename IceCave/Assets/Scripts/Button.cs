a2f8d244275c0ddef1c1ee8361e304ae56e9d9e0
KAMISENHEIMER <therealspydude@gmail.com>
2025-01-07 16:19:04 -0600

    fixed button being unpressed if you walk behind it

--- IceCave/Assets/Scripts/Button.cs
+++ IceCave/Assets/Scripts/Button.cs
@@ -2,20 +2,7 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
-    // The door that this button opens
-/*
-    public GameObject door;
-    Collider2D doorCollider;
-    
-    Animator animator;
-    Animator doorAnimator;
 
-    void Start()
-    {
-        doorCollider = door.GetComponent<Collider2D>();
-        animator = gameObject.GetComponent<Animator>();
-        doorAnimator = door.GetComponent<Animator>();
-*/
     public Door[] doors;
     public int colorIndex;
 
@@ -25,18 +12,12 @@ public class Button : MonoBehaviour
     {
         anim = GetComponent<Animator>();
         anim.SetFloat("Color", colorIndex);
-//>>>>>>> 65ba2b022d8ab8d4cc0de18d613c2314f9213fdc
     }
     
     void OnTriggerEnter2D(Collider2D otherCollider)
     {
         Debug.Log("Button Pressed");
-/*<<<<<<< HEAD
-        doorCollider.enabled = false;
-        animator.SetBool("buttonDown", true);
-        doorAnimator.SetBool("buttonDown", true);
 
-=======*/
         foreach(Door door in doors)
         {
             door.Open();
@@ -44,23 +25,23 @@ public class Button : MonoBehaviour
 
         
         anim.SetBool("buttonDown", true);
-//>>>>>>> 65ba2b022d8ab8d4cc0de18d613c2314f9213fdc
     }
     
     void OnTriggerExit2D(Collider2D otherCollider)
     {
+        //ensures if there is still something on the button, it doesnt try to open the doors
+        Collider2D[] thingsOnButton = Physics2D.OverlapPointAll(gameObject.transform.position);
+        if (thingsOnButton.Length > 1) {
+            return;
+        }
+        
         Debug.Log("Button Unpressed");
-/*<<<<<<< HEAD
-        doorCollider.enabled = true;
-        animator.SetBool("buttonDown", false);
-        doorAnimator.SetBool("buttonDown", false);
-=======*/
+
         foreach (Door door in doors)
         {
             door.TryClose();
         }
         anim.SetBool("buttonDown", false);
-//>>>>>>> 65ba2b022d8ab8d4cc0de18d613c2314f9213fdc
 
     }
 }
